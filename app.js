823ef07ffc209482fd1ac23a29235be010539bbe
Bethold <mtaycruz@gmail.com>
2021-02-04 22:00:11 -0500

    able to create html file with node

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
+
+var profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = (profileDataArr) => {
-  for(let i = 0; i < profileDataArr.length; i++){
-    console.log(profileDataArr[i]);
-  }
-  
-  console.log('======================');
-  
-  //is the same as this..
-  
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem);
-  });
-};
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if(err) throw new Error(err);
 
-printProfileData(profileDataArgs);
-
+  console.log("portfolio complete! Check out index.html to see the outpuit!");
+});
