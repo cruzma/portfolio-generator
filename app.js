89fc53ea59b1e505bd30136ce61fc625526a29a6
Bethold <mtaycruz@gmail.com>
2021-02-06 19:40:31 -0500

    added more functinality with node using inquirer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require("fs");
-// const generatePage = require('./src/page-template.js');
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -144,6 +144,12 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if(err) throw err;
+
+      console.log("portfolio complete! Check out index.html to see the outpuit!");
+    });
   });
   
